cdff22b53e5f245821145339be44316b62de3b58
Grodd91 <39410644+Grodd91@users.noreply.github.com>
2023-08-12 15:51:41 +0200

    Create main.py

---
+++ Algorithms/Miller-Rabin Primality Test/main.py
@@ -0,0 +1,28 @@
+import random
+
+def is_prime(n, k=5):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0:
+        return False
+
+    r, s = 0, n - 1
+    while s % 2 == 0:
+        r += 1
+        s //= 2
+
+    for _ in range(k):
+        a = random.randint(2, n - 1)
+        x = pow(a, s, n)
+        if x == 1 or x == n - 1:
+            continue
+        for _ in range(r - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
+            return False
+
+    return True
