a541de0f35887ba620936281a136a0640bb91c10
Grodd91 <39410644+Grodd91@users.noreply.github.com>
2023-08-12 15:36:37 +0200

    Create main2.py

---
+++ Algorithms/A* Algorithm/main2.py
@@ -0,0 +1,41 @@
+import heapq
+
+def astar_with_constraints(graph, start, goal, constraints):
+    open_list = [(0, start)]
+    came_from = {}
+    g_score = {vertex: float('infinity') for vertex in graph}
+    g_score[start] = 0
+    f_score = {vertex: float('infinity') for vertex in graph}
+    f_score[start] = heuristic(start, goal)
+
+    while open_list:
+        _, current = heapq.heappop(open_list)
+
+        if current == goal:
+            path = []
+            while current in came_from:
+                path.append(current)
+                current = came_from[current]
+            path.append(start)
+            path.reverse()
+            return path
+
+        for neighbor, weight in graph[current].items():
+            if not constraints(current, neighbor):
+                continue
+            tentative_g_score = g_score[current] + weight
+            if tentative_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = tentative_g_score
+                f_score[neighbor] = g_score[neighbor] + heuristic(neighbor, goal)
+                heapq.heappush(open_list, (f_score[neighbor], neighbor))
+
+    return None
+
+def heuristic(node, goal):
+    # Implement your heuristic function here
+    pass
+
+def constraints(node1, node2):
+    # Implement your constraint function here
+    pass
