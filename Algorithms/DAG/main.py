756082a46f40e17ee85a44d187edd8a8086bb32d
Grodd91 <39410644+Grodd91@users.noreply.github.com>
2023-08-12 15:37:24 +0200

    Create main.py

---
+++ Algorithms/DAG/main.py
@@ -0,0 +1,16 @@
+def topological_sort(graph):
+    visited = set()
+    stack = []
+
+    def dfs(node):
+        visited.add(node)
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                dfs(neighbor)
+        stack.append(node)
+
+    for node in graph:
+        if node not in visited:
+            dfs(node)
+
+    return stack[::-1]
