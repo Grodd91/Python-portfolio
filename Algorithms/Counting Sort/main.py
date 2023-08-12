9bc24aa5c9d6de667b9f484cc823997abeac1f8b
Grodd91 <39410644+Grodd91@users.noreply.github.com>
2023-08-12 15:40:58 +0200

    Create main.py

---
+++ Algorithms/Counting Sort/main.py
@@ -0,0 +1,9 @@
+def counting_sort(arr):
+    max_val = max(arr)
+    count = [0] * (max_val + 1)
+    for num in arr:
+        count[num] += 1
+    sorted_arr = []
+    for i, freq in enumerate(count):
+        sorted_arr.extend([i] * freq)
+    return sorted_arr
