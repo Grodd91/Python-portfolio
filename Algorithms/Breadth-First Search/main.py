697f4039876a17484fd45c0cbc7b31733b827a7f
Grodd91 <39410644+Grodd91@users.noreply.github.com>
2023-08-12 15:39:46 +0200

    Create main.py

---
+++ Algorithms/Breadth-First Search/main.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    while queue:
+        vertex = queue.popleft()
+        if vertex not in visited:
+            print(vertex)
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
