dba54ca2e01475db52ba39b1894544024b49804f
Grodd91 <39410644+Grodd91@users.noreply.github.com>
2023-08-12 15:41:40 +0200

    Create main.py

---
+++ Algorithms/Hashing/main.py
@@ -0,0 +1,22 @@
+class HashTable:
+    def __init__(self):
+        self.size = 100
+        self.table = [None] * self.size
+
+    def hash_function(self, key):
+        return sum(ord(char) for char in key) % self.size
+
+    def insert(self, key, value):
+        index = self.hash_function(key)
+        if self.table[index] is None:
+            self.table[index] = [(key, value)]
+        else:
+            self.table[index].append((key, value))
+
+    def get(self, key):
+        index = self.hash_function(key)
+        if self.table[index] is not None:
+            for k, v in self.table[index]:
+                if k == key:
+                    return v
+        raise KeyError(key)
