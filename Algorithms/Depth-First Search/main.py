045f6009b4754d568bb9397d428f435c5bf1eeb8
Grodd91 <39410644+Grodd91@users.noreply.github.com>
2023-08-12 15:40:37 +0200

    Create main.py

---
+++ Algorithms/Depth-First Search/main.py
@@ -0,0 +1,9 @@
+def dfs(graph, start):
+    visited = set()
+    stack = [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            print(vertex)
+            visited.add(vertex)
+            stack.extend(graph[vertex] - visited)
