c2341724553ee68c40b55f4e48149ba69993e792
Grodd91 <39410644+Grodd91@users.noreply.github.com>
2023-08-12 15:10:41 +0200

    Create main.py

---
+++ Algorithms/Longest Common Subsequence/main.py
@@ -0,0 +1,25 @@
+def longest_common_subsequence(X, Y):
+    m = len(X)
+    n = len(Y)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if X[i - 1] == Y[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    lcs = []
+    i, j = m, n
+    while i > 0 and j > 0:
+        if X[i - 1] == Y[j - 1]:
+            lcs.append(X[i - 1])
+            i -= 1
+            j -= 1
+        elif dp[i - 1][j] > dp[i][j - 1]:
+            i -= 1
+        else:
+            j -= 1
+
+    return ''.join(reversed(lcs))
