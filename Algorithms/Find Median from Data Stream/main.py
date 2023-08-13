e6d0a53c1f2bbdb002c0708249c61add9cdf61df
Grodd91 <39410644+Grodd91@users.noreply.github.com>
2023-08-13 16:37:54 +0200

    Create main.py

---
+++ Algorithms/Find Median from Data Stream/main.py
@@ -0,0 +1,23 @@
+import heapq
+
+class MedianFinder:
+    def __init__(self):
+        self.min_heap = []
+        self.max_heap = []
+
+    def add_num(self, num):
+        if not self.min_heap or num > self.min_heap[0]:
+            heapq.heappush(self.min_heap, num)
+        else:
+            heapq.heappush(self.max_heap, -num)
+
+        if len(self.min_heap) > len(self.max_heap) + 1:
+            heapq.heappush(self.max_heap, -heapq.heappop(self.min_heap))
+        elif len(self.max_heap) > len(self.min_heap):
+            heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
+
+    def find_median(self):
+        if len(self.min_heap) == len(self.max_heap):
+            return (self.min_heap[0] - self.max_heap[0]) / 2
+        else:
+            return self.min_heap[0]
