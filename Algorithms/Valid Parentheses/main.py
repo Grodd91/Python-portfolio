46f887f3fe84237063107aa839d7ff69ef1b6f3e
Grodd91 <39410644+Grodd91@users.noreply.github.com>
2023-08-13 16:28:43 +0200

    Create main.py

---
+++ Algorithms/Valid Parentheses/main.py
@@ -0,0 +1,13 @@
+def is_valid_parentheses(s):
+    stack = []
+    mapping = {')': '(', '}': '{', ']': '['}
+
+    for char in s:
+        if char in mapping:
+            top_element = stack.pop() if stack else '#'
+            if mapping[char] != top_element:
+                return False
+        else:
+            stack.append(char)
+
+    return not stack
