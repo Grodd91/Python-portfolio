df828166e542d753ff44e60e0bc8f698106bd269
Grodd91 <39410644+Grodd91@users.noreply.github.com>
2023-08-13 16:29:49 +0200

    Create main.py

---
+++ Algorithms/Sudoku Solver/main.py
@@ -0,0 +1,18 @@
+def is_valid(board, row, col, num):
+    for i in range(9):
+        if board[row][i] == num or board[i][col] == num or board[3 * (row // 3) + i // 3][3 * (col // 3) + i % 3] == num:
+            return False
+    return True
+
+def solve_sudoku(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == '.':
+                for num in map(str, range(1, 10)):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+                        if solve_sudoku(board):
+                            return True
+                        board[row][col] = '.'
+                return False
+    return True
