a647c379684d73d2ab95f6fd56f4c4c5b9205068
Grodd91 <39410644+Grodd91@users.noreply.github.com>
2023-08-13 16:39:38 +0200

    Create main.py

---
+++ Algorithms/Minimum Number of Arrows to Burst Balloons/main.py
@@ -0,0 +1,14 @@
+def find_min_arrows(points):
+    if not points:
+        return 0
+
+    points.sort(key=lambda x: x[1])
+    arrows = 1
+    end = points[0][1]
+
+    for i in range(1, len(points)):
+        if points[i][0] > end:
+            arrows += 1
+            end = points[i][1]
+
+    return arrows
