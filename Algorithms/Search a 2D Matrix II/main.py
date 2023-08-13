541d5775cb9e41f73181b347c5c9581479476571
Grodd91 <39410644+Grodd91@users.noreply.github.com>
2023-08-13 16:45:44 +0200

    Create main.py

---
+++ Algorithms/Search a 2D Matrix II/main.py
@@ -0,0 +1,16 @@
+def search_matrix(matrix, target):
+    if not matrix or not matrix[0]:
+        return False
+
+    rows, cols = len(matrix), len(matrix[0])
+    row, col = 0, cols - 1
+
+    while row < rows and col >= 0:
+        if matrix[row][col] == target:
+            return True
+        elif matrix[row][col] < target:
+            row += 1
+        else:
+            col -= 1
+
+    return False
