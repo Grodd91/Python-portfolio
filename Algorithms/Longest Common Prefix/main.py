d6963628606971a50775be39ce23b8e503878347
Grodd91 <39410644+Grodd91@users.noreply.github.com>
2023-08-13 16:27:28 +0200

    Create main.py

---
+++ Algorithms/Longest Common Prefix/main.py
@@ -0,0 +1,15 @@
+def longest_common_prefix(strs):
+    if not strs:
+        return ''
+
+    min_len = min(len(s) for s in strs)
+    prefix = ''
+
+    for i in range(min_len):
+        char = strs[0][i]
+        if all(s[i] == char for s in strs):
+            prefix += char
+        else:
+            break
+
+    return prefix
