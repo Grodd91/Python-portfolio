e42f51fa95f9300a224c74c8e33a30b5c2af52cb
Grodd91 <39410644+Grodd91@users.noreply.github.com>
2023-08-13 16:34:18 +0200

    Create main.py

---
+++ Algorithms/Climbing Stairs/main.py
@@ -0,0 +1,8 @@
+def climb_stairs(n):
+    if n == 1:
+        return 1
+    dp = [0] * (n + 1)
+    dp[1], dp[2] = 1, 2
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    return dp[n]
