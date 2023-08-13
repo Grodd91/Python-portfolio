559e948260dcb7e3e9f6f5d25516997d1be254e1
Grodd91 <39410644+Grodd91@users.noreply.github.com>
2023-08-13 16:43:45 +0200

    Create main.py

---
+++ Algorithms/Word Break/main.py
@@ -0,0 +1,12 @@
+def word_break(s, word_dict):
+    n = len(s)
+    dp = [False] * (n + 1)
+    dp[0] = True
+
+    for i in range(1, n + 1):
+        for j in range(i):
+            if dp[j] and s[j:i] in word_dict:
+                dp[i] = True
+                break
+
+    return dp[n]
