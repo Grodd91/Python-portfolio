91502e31b24aa4c008337ad576286bcb9fc4ffc2
Grodd91 <39410644+Grodd91@users.noreply.github.com>
2023-08-13 16:39:53 +0200

    Create main.py

---
+++ Algorithms/Find the Duplicate Number/main.py
@@ -0,0 +1,14 @@
+def find_duplicate(nums):
+    slow = fast = nums[0]
+    while True:
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        if slow == fast:
+            break
+
+    slow = nums[0]
+    while slow != fast:
+        slow = nums[slow]
+        fast = nums[fast]
+
+    return slow
