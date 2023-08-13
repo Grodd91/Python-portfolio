bb4dec5706a2b5fc0acbef989587d6909410f96c
Grodd91 <39410644+Grodd91@users.noreply.github.com>
2023-08-13 16:38:10 +0200

    Create main.py

---
+++ Algorithms/Kth Smallest Element in a BST/main.py
@@ -0,0 +1,17 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def kth_smallest(root, k):
+    stack = []
+    while root or stack:
+        while root:
+            stack.append(root)
+            root = root.left
+        root = stack.pop()
+        k -= 1
+        if k == 0:
+            return root.val
+        root = root.right
