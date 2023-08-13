b66d7e15581e7515c8170b1061dabdaddde33e84
Grodd91 <39410644+Grodd91@users.noreply.github.com>
2023-08-13 16:41:58 +0200

    Create main.py

---
+++ Algorithms/Lowest Common Ancestor of a Binary Tree/main.py
@@ -0,0 +1,16 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def lowest_common_ancestor(root, p, q):
+    if not root or root == p or root == q:
+        return root
+
+    left = lowest_common_ancestor(root.left, p, q)
+    right = lowest_common_ancestor(root.right, p, q)
+
+    if left and right:
+        return root
+    return left if left else right
