df341e978a48284a5d3249a4ab7f38e96842b121
Grodd91 <39410644+Grodd91@users.noreply.github.com>
2023-08-13 16:44:12 +0200

    Create main.py

---
+++ Algorithms/Maximal Square/main.py
@@ -0,0 +1,18 @@
+def maximal_square(matrix):
+    if not matrix:
+        return 0
+
+    rows, cols = len(matrix), len(matrix[0])
+    dp = [[0] * cols for _ in range(rows)]
+    max_side = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if matrix[i][j] == '1':
+                if i == 0 or j == 0:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+                max_side = max(max_side, dp[i][j])
+
+    return max_side * max_side
