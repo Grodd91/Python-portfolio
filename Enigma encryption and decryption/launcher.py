eca5ec1289b78c17e7eb1aff76a3a3a8a54d5dbe
Grodd91 <39410644+Grodd91@users.noreply.github.com>
2025-04-26 18:14:13 +0200

    Update launcher.py

--- Enigma encryption and decryption/launcher.py
+++ Enigma encryption and decryption/launcher.py
@@ -3,23 +3,24 @@ import subprocess
 
 def open_file(file_name):
     file_path = f"{file_name}.py"
+    if not os.path.exists(file_path):
+        print(f"Error: '{file_path}' does not exist.")
+        return
     try:
-        if os.path.exists(file_path):
-            subprocess.call(['python', file_path])
-        else:
-            print(f"File '{file_path}' does not exist.")
+        subprocess.call(['python', file_path])
     except Exception as e:
         print(f"Error occurred while opening the file: {str(e)}")
 
 def main():
     while True:
-        choice = input("Enter 'Enigma' to open enigma.py, 'KeyGenerator' to open KeyGenerator.py, or 'Q' to quit: ")
-        if choice.upper() == 'Q':
+        choice = input("Enter 'Enigma' to open enigma.py, 'KeyGenerator' to open KeyGenerator.py, or 'Q' to quit: ").strip().lower()
+        
+        if choice == 'q':
             print("Exiting the program.")
             break
-        elif choice.lower() == 'enigma':
+        elif choice == 'enigma':
             open_file('enigma')
-        elif choice.lower() == 'keygenerator':
+        elif choice == 'keygenerator':
             open_file('KeyGenerator')
         else:
             print("Invalid choice. Please enter 'Enigma', 'KeyGenerator', or 'Q'.")
